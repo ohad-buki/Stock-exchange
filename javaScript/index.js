e69f4297410f2e4e346d1429c00683a23340d768
ohad-buki <83958473+ohad-buki@users.noreply.github.com>
2021-06-22 12:34:11 +0300

    milestone3

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,6 +4,7 @@ const results = document.querySelector(".search-results");
 const spinner = document.querySelector(".spinner-border");
 const ul = document.createElement("ul");
 ul.classList.add("list-group", "list-group-flush");
+const input = document.querySelector(".form-control");
 
 async function getResults() {
   const res = await fetch(
@@ -12,18 +13,65 @@ async function getResults() {
   const data = await res.json();
   return data;
 }
+
+async function getCompData(S) {
+  const res = await fetch(
+    `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${S}`
+  );
+  const compData = await res.json();
+  return compData;
+}
+
 function addToScreen() {
   spinner.classList.remove("d-none");
+  ul.innerHTML = "";
   getResults().then((data) => {
-    for (let i = 0; i <= 10; i++) {
-      ul.innerHTML += `<li><a href="./html/company.html?symbol=${data[i].symbol}" class="link-primary list-group-item">${data[i].name},(${data[i].symbol})</a></li>`;
+    if (input.value == "") {
+      for (let i = 0; i <= 10; i++) {
+        getCompData(data[i].symbol).then((fullCompData) => {
+          let color = "red";
+          if (fullCompData.profile.changesPercentage[1] == "+") {
+            color = "green";
+          }
+          ul.innerHTML += `<li><a href="./html/company.html?symbol=${data[i].symbol}" class="link-primary list-group-item"> <img src="${fullCompData.profile.image}" alt="Logo"><span class = "comp-name">${data[i].name},(${data[i].symbol})</span><span class = "list-price">$${fullCompData.profile.price}</span> <span class = ${color}>${fullCompData.profile.changesPercentage}</span></a></li>`;
+        });
+      }
+    } else {
+      for (let j = 0; j < data.length; j++) {
+        if (data[j].name == null) {
+          continue;
+        }
+        if (
+          data[j].symbol.includes(input.value) ||
+          data[j].name.includes(input.value)
+        ) {
+          getCompData(data[j].symbol).then((fullCompData) => {
+            let color = "red";
+            if (fullCompData.profile.changesPercentage[1] == "+") {
+              color = "green";
+            }
+            ul.innerHTML += `<li><a href="./html/company.html?symbol=${data[j].symbol}" class="link-primary list-group-item"> <img src="${fullCompData.profile.image}" alt="Logo"><span class = "comp-name">${data[j].name},(${data[j].symbol})</span><span class = "list-price">$${fullCompData.profile.price}</span> <span class = ${color}>${fullCompData.profile.changesPercentage}</span></a></li>`;
+          });
+        }
+      }
     }
     spinner.classList.add("d-none");
     results.appendChild(ul);
   });
 }
 
+function debounce(func, timeout = 400) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
 button.addEventListener("click", (event) => {
-  ul.innerHTML = "";
   addToScreen();
 });
+
+input.addEventListener("keyup", debounce(addToScreen));
